c992a2e4ec546627cca6ea61a2457538349ced94
ccarroll929 <146889005+ccarroll929@users.noreply.github.com>
2024-01-25 18:38:26 -0600

    added array for questions/user input

--- index.js
+++ index.js
@@ -1,7 +1,86 @@
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "What is your project title?",
+        validate: validateInput,
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Enter a description of your project",
+        validate: validateInput,
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "How can others use this project?",
+        validate: validateInput,
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "Enter software installation info or commands needed to access your project",
+        validate: validateInput,
+
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "Please select a license for this project",
+        choices: [
+            "GNU AGPLv3",
+            "GNU GPLv3",
+            "GNU LGPLv3",
+            "Apache 2.0",
+            "Boost Software 1.0",
+            "MIT",
+            "Mozilla",
+        ],
+        validate: validateInput,
+    },
+    {
+        type: "input",
+        name: "contributing",
+        message: "How can users contribute to your project?",
+        validate: validateInput,
+    },
+    {
+        type: "input",
+        name: "userName",
+        message: "What is your GitHub username?",
+        validate: validateInput,
+    },
+    {
+        type: "input",
+        name: "tests",
+        message: "Please enter any testing instructions you would like to provide for this project.",
+        validate: validateInput,
+    },
+    {
+        type: "input",
+        name: "userName",
+        message: "What is your GitHub username?",
+        validate: validateInput,
+    },
+
+    // QUESTIONS section -- email address
+    {
+        type: "input",
+        name: "userEmail",
+        message: "What is your GitHub email address that contributors may contact?",
+        validate: function (value) {
+            if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(value)) {
+                return true;
+            } else {
+                return "Not a valid email address. Please enter a valid email address.";
+            }
+        },
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
